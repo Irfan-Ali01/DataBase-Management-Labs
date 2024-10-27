eac98185f53e440ef6e24ff4cb0d3e478e67210e
Irfan Ali <151863237+Irfan-Ali01@users.noreply.github.com>
2024-10-27 16:33:04 +0500

    Add files via upload

---
+++ Courses.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM school.courses
+LIMIT 0, 1000
+
+-- Date: 2024-10-27 16:32
+*/
+INSERT INTO `` (`C_no`,`C_name`,`C_Teacher`,`C_credit_Hours`) VALUES ('1','Programming Fundamentals','Sir Awais','4');
+INSERT INTO `` (`C_no`,`C_name`,`C_Teacher`,`C_credit_Hours`) VALUES ('2','OOPS','Sir Irfan','3');
+INSERT INTO `` (`C_no`,`C_name`,`C_Teacher`,`C_credit_Hours`) VALUES ('3','Database System','Sir Ali','4');
+INSERT INTO `` (`C_no`,`C_name`,`C_Teacher`,`C_credit_Hours`) VALUES ('4','Data Structure','Sir Zeeshan','4');
+INSERT INTO `` (`C_no`,`C_name`,`C_Teacher`,`C_credit_Hours`) VALUES ('5','Software Engineering','Sir Hammad','4');
---
+++ st_info.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM school.st_info
+LIMIT 0, 1000
+
+-- Date: 2024-10-27 16:32
+*/
+INSERT INTO `` (`St_no`,`Admission_Date`,`St_Name`,`St_Age`,`St_Address`) VALUES ('1','0200-02-24','Irfan',20,'Pakistan');
+INSERT INTO `` (`St_no`,`Admission_Date`,`St_Name`,`St_Age`,`St_Address`) VALUES ('2','2000-05-25','Zeeshan',21,'Pakistan');
+INSERT INTO `` (`St_no`,`Admission_Date`,`St_Name`,`St_Age`,`St_Address`) VALUES ('3','2004-06-12','Zohaib',14,'Pakistan');
+INSERT INTO `` (`St_no`,`Admission_Date`,`St_Name`,`St_Age`,`St_Address`) VALUES ('4','2006-06-20','Hammad',14,'Pakistan');
+INSERT INTO `` (`St_no`,`Admission_Date`,`St_Name`,`St_Age`,`St_Address`) VALUES ('5','2008-05-10','Ali',22,'Pakistan');

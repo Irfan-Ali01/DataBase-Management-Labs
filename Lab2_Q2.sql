016f590ba8a9ce9a542b056ef21bcc2e8d2b62f1
Irfan Ali <151863237+Irfan-Ali01@users.noreply.github.com>
2024-10-29 23:37:55 +0500

    Add files via upload

---
+++ Lab2_Q2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employee
+(
+Emp_id varchar(20),
+Name char (20),
+Age  int,
+R_Address varchar(25),
+Salary float
+)
---
+++ Lab2_Q3.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee
+VALUES
+("Emp_01","Danish",28,"Nawabshah",20000),
+("Emp_02","Khuram",32,"Hyderabad",15000),
+("Emp_03","Ahmed",25,"Karachi",20000),
+("Emp_04","Adnan",23,"Sukkur",65000),
+("Emp_05","Bilal",27,"Larkana",30000),
+("Emp_06","Imran",22,"Larkana",40000),
+("Emp_07","Ayaz",24,"Karachi",50000)
